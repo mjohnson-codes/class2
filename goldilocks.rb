1184c3082338a548ded894bfe676ed3ce11187ee
mjohnson-codes <mjohnson@trialnetwork.com>
2015-05-25 20:03:51 -0400

    goldilocks

--- goldilocks.rb
+++ goldilocks.rb
@@ -4,6 +4,11 @@ food = gets.chomp.downcase
 puts "What temperature is your food in Farenheit? "
 temp_f = gets.chomp.to_i
 
+temp_c = (temp_f - 32) * (5.0/ 9.0)
+puts "Your #{food} is #{temp_c} degrees Celsius\n\n"
+
+
+
 case food
 when "porridge"
 	puts "Yum, Porridge!!"
@@ -12,6 +17,9 @@ when "broccoli", "beets", "olives"
 else
 	puts "What is this food???"
 end
+
+
+  
 
 if temp_f < 100 && temp_f > 80
 	puts "Goldilocks, the temp is just right!!"
@@ -25,8 +33,4 @@ else
 	puts "Run away!!!"
 end
 
-=begin
-temp_c = (temp_f - 32) * (5.0/ 9.0)
-puts "Your porridge is #{temp_c} degrees Celsius\n\n"
-puts "Thanks for playing!!\n\n"
-=end
+puts "\n\nThanks for playing!!\n\n"
